ea8bfb4f249c43566da95c7295b1d48a42da65e7
Sovannjet <sovannje.321777@nv.ccsd.net>
2021-07-27 16:32:32 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,81 @@
+ROWS = 6
+COLUMNS = 7
+
+
+def printboard():
+    for row in board:
+        for col in row:
+            print(col + " ", end='')
+        print()
+
+
+def placecoin():
+    rowin = len(board) - 1
+    while board[rowin][colin] != "-":
+        rowin -= 1
+    if rowin < 0:
+        print("That column is full.")
+    else:
+        board[rowin][colin] = pnum
+
+
+def fourinarow():  # checks for 4-in-a-rows on the board
+    # loop from bottom to top, left to right
+    for r in range(len(board)-1, -1, -1):  # 5, 4, 3, 2, 1, 0
+        for c in range(COLUMNS):
+            if c <= 3:  # coin is in left half of board
+                if checkforhorizline(r, c) or checkforbltrline(r, c):
+                    return True
+            if c >= 3:  # coin is in right half of board
+                if checkforbrtlline(r, c):
+                    return True
+            if r >= 3:  # coin is in bottom half of board
+                if checkforvertline(r, c):
+                    return True
+    return False
+
+
+def checkforhorizline(r, c):
+    if board[r][c] != "-":
+        if board[r][c] == board[r][c+1] == board[r][c+2] == board[r][c+3]:
+            return True
+    return False
+
+
+def checkforvertline(r, c):
+    if board[r][c] != "-":
+        if board[r][c] == board[r-1][c] == board[r-2][c] == board[r-3][c]:
+            return True
+    return False
+
+
+def checkforbltrline(r, c):
+    if board[r][c] != "-":
+        if board[r][c] == board[r-1][c+1] == board[r-2][c+2] == board[r-3][c+3]:
+            return True
+    return False
+
+
+def checkforbrtlline(r, c):
+    if board[r][c] != "-":
+        if board[r][c] == board[r-1][c-1] == board[r-2][c-2] == board[r-3][c-3]:
+            return True
+    return False
+
+
+board = [["-" for i in range(COLUMNS)] for j in range(ROWS)]  # create 7x6 connect-4 board
+printboard()
+
+turnnum = 0
+while fourinarow() is False:
+    pnum = str(turnnum % 2 + 1)  # player number
+    colinstr = input("Player " + pnum + ", type the column (1-7) you want to place your coin in: ")
+    if colinstr in (str(n) for n in range(1, 8)):  # proceed if the input is a number from 1-7
+        colin = int(colinstr) - 1  # the actual integer for the column input
+        placecoin()
+        printboard()
+        if turnnum >= 6 and fourinarow() is True:  # only check for 4-in-a-row if there have been at least 7 turns
+            print("Player " + pnum + " wins!")
+        turnnum += 1
+    else:
+        print("Make sure it's one number from 1 to 7.")
